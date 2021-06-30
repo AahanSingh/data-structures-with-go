8a76e750f19b4e0ec3b8f2bc58b9474a3b50952c
AahanSingh <aahansingh95@gmail.com>
2021-06-30 22:38:33 +0530

    Added entrypoint functions.

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/aahansingh/data-structures-with-go/doublylinkedlist"
 	"github.com/aahansingh/data-structures-with-go/linkedlist"
 )
 
 func main() {
+	fmt.Println("---------- LINKED LIST ---------- ")
 	linkedlist.RunDemo()
+	fmt.Println("---------- DOUBLY LINKED LIST ---------- ")
+	doublylinkedlist.RunDemo()
 }

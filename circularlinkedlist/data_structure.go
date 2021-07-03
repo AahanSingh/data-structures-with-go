fc87844a73f4bec0ded799e47978a0b8690a3db0
AahanSingh <aahansingh95@gmail.com>
2021-07-03 12:16:25 +0530

    Added basic structure for circular linked lists.

---
+++ circularlinkedlist/data_structure.go
@@ -0,0 +1,41 @@
+package circularlinkedlist
+
+import "fmt"
+
+// This is the same as the struct for a singly linked list.
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func DisplayList(head *Node) {
+	fmt.Printf("The list is: ")
+	current := head
+	if current == nil {
+		fmt.Println("empty.")
+		return
+	} else {
+		fmt.Print(" -> ", current)
+		current = current.Next
+		for ; current != head; current = current.Next {
+			fmt.Print(" -> ", current)
+		}
+		fmt.Println()
+		fmt.Println()
+	}
+}
+
+func Length(head *Node) int {
+	current := head
+	if current == nil {
+		return 0
+	} else {
+		len := 1
+		current = current.Next
+		for ; current != head; current = current.Next {
+			len++
+			current = current.Next
+		}
+		return len
+	}
+}
